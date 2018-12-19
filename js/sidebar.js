98a666c369c3f3de614bb899408e4c0346155c06
gnems <g_nemsveridze@cu.edu.ge>
2018-12-19 23:32:35 +0400

    Navbar implementation

---
+++ js/sidebar.js
@@ -0,0 +1,20 @@
+isOpen = false;
+
+function openNav() {
+    isOpen = true;
+    document.getElementById("mySidenav").style.width = "900px";
+    document.getElementById("main").style.marginLeft = "900px";
+}
+  
+function closeNav() {
+    isOpen = false;
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.margin= "0 auto";
+}
+
+function toggleNav(){
+    if (isOpen)
+        closeNav();
+    else
+        openNav();
+}
